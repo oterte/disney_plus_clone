9d221938789e18c0783490bb493d0d133313ff87
oterte <97093479+oterte@users.noreply.github.com>
2023-08-04 20:12:25 +0900

    axios instance 생성 및 요청용 request 생성

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https//api/themoviedb.org/3",
+  params: {
+    api_key: "3c445cadc2cfe27a003e07266d753f17",
+    language: "ko-kr",
+  },
+});
+
+export default instance;
