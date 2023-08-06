b3c33555c48199b581c7478beecbd2c014bce101
oterte <97093479+oterte@users.noreply.github.com>
2023-08-06 18:44:17 +0900

    비디오 배너부분 스타일링 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
-import Nav from './components/Nav';
+import { styled } from "styled-components";
+import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Nav />
-    </div>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+
+  /* Nav height이 70이라 그거보다 아래로 */
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
