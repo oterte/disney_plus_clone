8e6e9082b65673587de00a456e5857121c6ccadc
oterte <97093479+oterte@users.noreply.github.com>
2023-08-07 19:51:43 +0900

    이미지 배너 추가, 몇몇 부분에서 오류 발생

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,33 +1,57 @@
 import React, { useEffect, useState } from "react";
 import instance from "../api/axios";
 import request from "../api/request";
-import axios from "axios";
+import "./Banner.css";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
-  const fetchData = async () => {
-    // 현재 영화 정보 가져오기(여러개)
-    const data = await instance.get(request.fetchNowPlaying);
-    console.log(data);
-
-    // 여러 영화 중 하나의 id 가져오기
-    const movieId =
-      data.data.results[Math.floor(Math.random() * data.data.results.length)]
-        .id;
-
-    //  특정 영화 상세정보 가져오기
-    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
-      params: { append_to_response: "videos" },
-    });
-
-    setMovie(movieDetail);
-  };
 
   useEffect(() => {
     fetchData();
   }, []);
+  const fetchData = async () => {
+    // 현재 상영중인 영화 정보를 가져오기(여러 영화)
+    const response = await instance.get(request.fetchNowPlaying);
+    // 여러 영화 중 영화 하나의 ID를 가져오기
+    const movieId =
+      response.data.results[
+        Math.floor(Math.random() * response.data.results.length)
+      ].id;
 
-  return <div>Banner</div>;
+    // 특정 영화의 더 상세한 정보를 가져오기(비디오 정보도 포함)
+    const { data: movieDetail } = await instance.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+    setMovie(movieDetail);
+    console.log(movie);
+  };
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  };
+  console.log(movie);
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="baneer__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__button play">Play</button>
+          )}
+        </div>
+        <p className="banner__description">{truncate(movie.overview, 100)}</p>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 };
 
 export default Banner;
