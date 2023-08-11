e616d2332872ce938d57ae20671c8fbf38ad4e93
oterte <97093479+oterte@users.noreply.github.com>
2023-08-11 17:36:32 +0900

    영화 나열을 위한 Row 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Banner from "./components/Banner";
 import Category from "./components/Category";
+import Row from "./components/Row";
+import request from "./api/request";
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
       <Nav />
       <Banner />
       <Category />
+      <Row title="Trending Now" id="TN" fetchUrl={request.fetchTrending}/>
+      <Row title="Top Rated" id="TR" fetchUrl = {request.fetchTopRated}/>
+      <Row title="Action Moives" id="AM" fetchUrl= {request.fetchActionMovies}/>
+      <Row title="Comedy Movies" id="CM" fetchUrl = {request.fetchComedyMovies}/>
     </Container>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from "react";
+import instance from "../api/axios";
 
-const Row = () => {
+const Row = ({ title, id, fetchUrl }) => {
+  const [movies, setMovies] = useState();
+  // 함수를 메모이제이션 하기 위해 useCallback 사용하기
+  // fetchUrl이 바뀔때만 렌더링 시 함수 생성
+  const fetchMovieData = useCallback(async () => {
+    const request = await instance.get(fetchUrl);
+    setMovies(request.data.results);
+    return request;
+  }, [fetchUrl]);
+
+  const handleClick = () => {
+    // 모달 기능 구현 필요
+  };
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchMovieData]);
+
   return (
-    <div>Row</div>
-  )
-}
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((item) => (
+            <img
+              key={item.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
+              alt={item.name}
+              onClick={() => handleClick(item)}
+            />
+          ))}
+        </div>
+        <div className="slier__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Row
+export default Row;
