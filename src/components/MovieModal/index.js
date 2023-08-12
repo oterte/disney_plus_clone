082ae5068cedd5a2f076a69fbe2b1900d8acb37a
oterte <97093479+oterte@users.noreply.github.com>
2023-08-12 14:17:01 +0900

    모달 컴포넌트 UI 추가

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieModal.css";
 
 const MovieModal = ({
@@ -11,7 +11,28 @@ const MovieModal = ({
   vote_average,
   setModalOpen,
 }) => {
-  return <div>MovieModal</div>;
+  const [modalVisibility, setModalVisibility] = useState(true);
+  return (
+    <div className="presentation" role="presentation">
+      <div className="wrapper-modal">
+        <span onClick={() => setModalVisibility(false)}>X</span>
+        <img
+          className="modal__poster-img"
+          src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+          alt="modal-img"
+        />
+        <div className="modal__content">
+          <p className="modal__details">
+            <span className="modal__user_perc">100% for you</span>{" "}
+            {release_date ? release_date : first_air_date}
+          </p>
+          <h2 className="modal__title">{title ? title : name}</h2>
+          <p className="modal__overview">평점: {vote_average}</p>
+          <p className="modal__overview">{overview}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieModal;
