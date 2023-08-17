892b9ca2e5389056faf7da629a7f68606ce947ab
oterte <97093479+oterte@users.noreply.github.com>
2023-08-17 16:32:48 +0900

    modal 부분 스타일링 추가

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./MovieModal.css";
 
 const MovieModal = ({
@@ -11,24 +11,30 @@ const MovieModal = ({
   vote_average,
   setModalOpen,
 }) => {
-  const [modalVisibility, setModalVisibility] = useState(true);
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <span onClick={() => setModalVisibility(false)}>X</span>
-        <img
-          className="modal__poster-img"
-          src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
-          alt="modal-img"
-        />
-        <div className="modal__content">
-          <p className="modal__details">
-            <span className="modal__user_perc">100% for you</span>{" "}
-            {release_date ? release_date : first_air_date}
-          </p>
-          <h2 className="modal__title">{title ? title : name}</h2>
-          <p className="modal__overview">평점: {vote_average}</p>
-          <p className="modal__overview">{overview}</p>
+        <div className="modal">
+          <span
+            className="modal-close"
+            onClick={() => setModalOpen(false)}
+          >
+            X
+          </span>
+          <img
+            className="modal__poster-img"
+            src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+            alt="modal-img"
+          />
+          <div className="modal__content">
+            <p className="modal__details">
+              <span className="modal__user_perc">100% for you</span>{" "}
+              {release_date ? release_date : first_air_date}
+            </p>
+            <h2 className="modal__title">{title ? title : name}</h2>
+            <p className="modal__overview">평점: {vote_average}</p>
+            <p className="modal__overview">{overview}</p>
+          </div>
         </div>
       </div>
     </div>
