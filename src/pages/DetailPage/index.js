5a6a6ca8c07ba281ece2904547e11805df320358
oterte <97093479+oterte@users.noreply.github.com>
2023-08-21 17:04:45 +0900

    useParams 이용해서 detailpage 추가

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import instance from "../../api/axios";
 
 const DetailPage = () => {
-  return (
-    <div>DetailPage</div>
-  )
-}
+  const { movieId } = useParams();
 
-export default DetailPage
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const request = await instance.get(
+        `https://api.themoviedb.org/3/movie/${movieId}`
+      );
+      setMovies(request.data);
+    }
+    fetchData();
+  }, [movieId]);
+
+  if (!movies) return null;
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movies.backdrop_path}`}
+        alt="img"
+      />
+    </section>
+  );
+};
+
+export default DetailPage;
